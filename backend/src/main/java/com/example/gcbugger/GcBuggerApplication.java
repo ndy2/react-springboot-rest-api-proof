eb6dc9dab218fd9bee07195acafabad52325f6e4
ndy2 <emrdbs12@gmail.com>
2022-04-29 16:07:55 +0900

    initial commit gc-bugger application

---
+++ backend/src/main/java/com/example/gcbugger/GcBuggerApplication.java
@@ -0,0 +1,13 @@
+package com.example.gcbugger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcBuggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GcBuggerApplication.class, args);
+    }
+
+}
